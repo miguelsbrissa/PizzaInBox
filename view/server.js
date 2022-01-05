734ae545b062551ed85a7c6c3d66a6d47eb8fcf8
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-01-05 01:08:27 -0300

    Change server port for 8081
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- view/server.js
+++ view/server.js
@@ -7,7 +7,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const port = 80;
+const port = 8081;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/js'))
